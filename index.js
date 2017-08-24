9c1afb668fe5b960074e8eb344e02c7a3192f01f
toure <tourekadiy@gmail.com>
2017-08-24 11:08:27 +0200

    handler; installation of http-auth;

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require('body-parser');
 let app = express();
-//app.use(express.static);
+app.use(express.static('public'));
+//handler url + fonction
+app.post("/auth",
+    //1ère fction à être appellée
+    bodyParser.urlencoded({ extended: true }),
+    function(request, response) {
+        console.log(request.body);
+        response.send("Success!")
+    });
+
 app.listen(80, function() {
     console.log('Server listening on port 80...');
 });
